fe37e1b75dd0c81d3ee6136db34ef71e1f76e226
asepmoels <asepmoels@gmail.com>
2021-07-23 14:25:20 +0700

    Update Google Sign In api to version 6.0

--- HSGoogleDrivePicker/HSGoogleDrivePicker/HSDrivePicker.swift
+++ HSGoogleDrivePicker/HSGoogleDrivePicker/HSDrivePicker.swift
@@ -4,97 +4,98 @@ import UIKit
 
 /// Navigation controller to present the File Viewer and signin controller
 @objcMembers open class HSDrivePicker: UINavigationController {
-    /** Provide your API secret
-     Note that the client ID is read from your GoogleService-Info.plist
-     **/
-    
-    
-    /** Present the picker from your view controller. It will present as a modal form.
-     The completion returns both the file, and the authorised manager which can be used to download the file **/
-    
-    
-    /*
-     
-     Appearance can mostly be managed through the appearance proxy.
-     e.g.  [[UINavigationBar appearance] setBackgroundImage: <your image> ];
-     
-     or to style the segmented control (which is addmittedly wierd)
-     
-     //selected text
-     [[UISegmentedControl appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor redColor]} forState:UIControlStateSelected];
-     //not selected text
-     [[UISegmentedControl appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor greenColor]} forState:UIControlStateNormal];
-     //background
-     [[UIImageView appearanceWhenContainedIn:[UISegmentedControl class],nil] setTintColor:[UIColor blueColor]];
-     
-     */
-    
-    
-    //*specify status bar style. Default is UIStatusBarStyleDefault *
-    
-    
-    /**
-     Handle the url callback from google authentication
-     
-     @param url the callback url
-     */
-    
-    
-    private var viewer: HSDriveFileViewer?
- 
-    public class func handle(_ url: URL?) -> Bool {
-        _ = HSGIDSignInHandler.sharedInstance
-        if GIDSignIn.sharedInstance().handle(url) {
-            return true
+  /** Provide your API secret
+   Note that the client ID is read from your GoogleService-Info.plist
+   **/
+
+
+  /** Present the picker from your view controller. It will present as a modal form.
+   The completion returns both the file, and the authorised manager which can be used to download the file **/
+
+
+  /*
+
+   Appearance can mostly be managed through the appearance proxy.
+   e.g.  [[UINavigationBar appearance] setBackgroundImage: <your image> ];
+
+   or to style the segmented control (which is addmittedly wierd)
+
+   //selected text
+   [[UISegmentedControl appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor redColor]} forState:UIControlStateSelected];
+   //not selected text
+   [[UISegmentedControl appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor greenColor]} forState:UIControlStateNormal];
+   //background
+   [[UIImageView appearanceWhenContainedIn:[UISegmentedControl class],nil] setTintColor:[UIColor blueColor]];
+
+   */
+
+
+  //*specify status bar style. Default is UIStatusBarStyleDefault *
+
+
+  /**
+   Handle the url callback from google authentication
+
+   @param url the callback url
+   */
+
+
+  private var viewer: HSDriveFileViewer?
+
+  public class func handle(_ url: URL?) -> Bool {
+    _ = HSGIDSignInHandler.sharedInstance
+    if let url = url,
+      GIDSignIn.sharedInstance.handle(url) {
+      return true
+    }
+
+    return false
+  }
+
+  public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+  }
+
+  public init() {
+    let viewer = HSDriveFileViewer()
+
+    super.init(rootViewController: viewer)
+    modalPresentationStyle = UIModalPresentationStyle.pageSheet
+    self.viewer = viewer
+  }
+
+  required public init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+
+  public func pick(from vc: UIViewController?, withCompletion completion: @escaping (_ manager: HSDriveManager?, _ file: GTLRDrive_File?) -> Void) {
+    viewer?.completion = completion
+    viewer?.shouldSignInOnAppear = true
+
+    vc?.present(self, animated: true)
+
+  }
+
+  func downloadFileContent(withService service: GTLRDriveService?, file: GTLRDrive_File?, completionBlock: @escaping (Data?, Error?) -> Void) {
+
+    guard let downloadURL = file?.downloadURL else {
+      completionBlock(nil, NSError(domain: NSURLErrorDomain, code: NSURLErrorBadURL, userInfo: nil))
+      return
+    }
+    let fetcher = service?.fetcherService.fetcher(with: downloadURL)
+
+    fetcher?.beginFetch(completionHandler: { data, error in
+      if error == nil {
+        // Success.
+        completionBlock(data, nil)
+      } else {
+        if let error = error {
+          print("An error occurred: \(error)")
         }
-        
-        return false
-    }
-    
-    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-    
-    public init() {
-        let viewer = HSDriveFileViewer()
+        completionBlock(nil, error!)
+      }
+    })
 
-        super.init(rootViewController: viewer)
-        modalPresentationStyle = UIModalPresentationStyle.pageSheet
-        self.viewer = viewer
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    
-    public func pick(from vc: UIViewController?, withCompletion completion: @escaping (_ manager: HSDriveManager?, _ file: GTLRDrive_File?) -> Void) {
-        viewer?.completion = completion
-        viewer?.shouldSignInOnAppear = true
-        
-        vc?.present(self, animated: true)
-        
-    }
-    
-    func downloadFileContent(withService service: GTLRDriveService?, file: GTLRDrive_File?, completionBlock: @escaping (Data?, Error?) -> Void) {
-        
-        guard let downloadURL = file?.downloadURL else {
-            completionBlock(nil, NSError(domain: NSURLErrorDomain, code: NSURLErrorBadURL, userInfo: nil))
-            return
-        }
-            let fetcher = service?.fetcherService.fetcher(with: downloadURL)
-            
-        fetcher?.beginFetch(completionHandler: { data, error in
-                if error == nil {
-                    // Success.
-                    completionBlock(data, nil)
-                } else {
-                    if let error = error {
-                        print("An error occurred: \(error)")
-                    }
-                    completionBlock(nil, error!)
-                }
-            })
-
-    }
+  }
 }
--- HSGoogleDrivePicker/HSGoogleDrivePicker/HSGIDSignInHandler.swift
+++ HSGoogleDrivePicker/HSGoogleDrivePicker/HSGIDSignInHandler.swift
@@ -5,117 +5,114 @@ import Foundation
 import GoogleAPIClientForREST
 import GoogleSignIn
 
-open class HSGIDSignInHandler: NSObject, GIDSignInDelegate {
-    
-    @objc static let hsGIDSignInChangedNotification = NSNotification.Name("HSGIDSignInChangedNotification")
-    @objc static let hsGIDSignInFailedNotification = NSNotification.Name("HSGIDSignInFailedNotification")
-    
-    static let sharedInstance = HSGIDSignInHandler()
-    
-    
-    class var authoriser:GTMFetcherAuthorizationProtocol? {
-        return HSGIDSignInHandler.sharedInstance.authoriser
-    }
-    
-    class func canAuthorise() -> Bool {
-        if HSGIDSignInHandler.sharedInstance.authoriser?.canAuthorize == true {
-            return true
-        }
-        
-        return false
-    }
-    
-    weak var viewController:UIViewController?
-    class func signIn(from vc: UIViewController?) {
-        
-        let handler = self.sharedInstance
-        handler.viewController = vc
-        
-        //in iOS 8, the sign-in is called with view_did_appear before the signIn_didSignIn is fired on a queue
-        DispatchQueue.main.async(execute: {
-            
-            if let vc = vc {
-                GIDSignIn.sharedInstance().presentingViewController = vc
-            }
-            GIDSignIn.sharedInstance().signIn()
-        })
-        
-    }
-    
-    class func signOut() {
-        GIDSignIn.sharedInstance().disconnect()
-        GIDSignIn.sharedInstance().signOut()
-    }
-    
-    var authoriser: GTMFetcherAuthorizationProtocol?
-    
-    override init() {
-        super.init()
-        guard let signIn = GIDSignIn.sharedInstance() else {
-            print("Unable to create sign in instance")
-            return
-        }
-        
-        if signIn.clientID == nil {
-            signIn.clientID = clientIDFromPlist
-        }
-        
-        signIn.delegate = self
-        
-        let currentScopes = GIDSignIn.sharedInstance().scopes
-        let newScopes = (currentScopes ?? []) + [kGTLRAuthScopeDrive]
-        signIn.scopes = newScopes
-        
-        signIn.restorePreviousSignIn()
-    }
-    
-    
-    /// Either add GoogleService-Info.plist to your project
-    /// or manually initialise Google Signin by calling
-    /// GIDSignIn.sharedInstance().clientID = "YOUR_CLIENT_ID" in your AppDelegate
-    var clientIDFromPlist:String {
-        let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
-        if let dict = NSDictionary(contentsOfFile: path ?? "") as? [String:Any]? {
-            let clientID = dict?["CLIENT_ID"] as? String
-            if let clientID = clientID  {
-                return clientID
-            }
-        }
-        
-        fatalError("GoogleService-Info.plist hasn't been added to the project")
-    }
-    
-    
-    
-    public func sign(_ signIn: GIDSignIn?, didSignInFor user: GIDGoogleUser?, withError error: Error?) {
-        
-        if error == nil {
-            authoriser = user?.authentication.fetcherAuthorizer()
-            NotificationCenter.default.post(name: HSGIDSignInHandler.hsGIDSignInChangedNotification, object: self)
-        } else {
-            authoriser = nil
-            NotificationCenter.default.post(name: HSGIDSignInHandler.hsGIDSignInFailedNotification, object: self)
+open class HSGIDSignInHandler: NSObject {
 
-            //silent signin generates this error
-            if let code = (error as NSError?)?.code {
-                if code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
-                    return
-                }
-            }
- 
-            if let viewController = viewController {
-                let alert = UIAlertController.init(title: "Unable to sign in to Drive",
-                                                   message: error?.localizedDescription,
-                                                   preferredStyle: .alert)
-                alert.addAction(UIAlertAction.init(title: "OK", style: .default))
-                viewController.present(alert, animated: true)
-            }
-        }
+  @objc static let hsGIDSignInChangedNotification = NSNotification.Name("HSGIDSignInChangedNotification")
+  @objc static let hsGIDSignInFailedNotification = NSNotification.Name("HSGIDSignInFailedNotification")
+
+  static let sharedInstance = HSGIDSignInHandler()
+
+
+  class var authoriser:GTMFetcherAuthorizationProtocol? {
+    return HSGIDSignInHandler.sharedInstance.authoriser
+  }
+
+  class func canAuthorise() -> Bool {
+    if HSGIDSignInHandler.sharedInstance.authoriser?.canAuthorize == true {
+      return true
     }
-    
-    public func sign(_ signIn: GIDSignIn?, didDisconnectWith user: GIDGoogleUser?, withError error: Error?) {
-        print("User disconnected")
-        authoriser = nil
+
+    return false
+  }
+
+  weak var viewController:UIViewController?
+  class func signIn(from vc: UIViewController?) {
+
+    let handler = self.sharedInstance
+    handler.viewController = vc
+
+    //in iOS 8, the sign-in is called with view_did_appear before the signIn_didSignIn is fired on a queue
+    DispatchQueue.main.async(execute: {
+      guard let caller = vc else { return }
+      let config = GIDConfiguration(clientID: self.sharedInstance.clientIDFromPlist)
+      GIDSignIn.sharedInstance.signIn(with: config, presenting: caller) { user, error in
+        self.sharedInstance.sign(GIDSignIn.sharedInstance, didSignInFor: user, withError: error)
+      }
+    })
+
+  }
+
+  class func signOut() {
+    GIDSignIn.sharedInstance.disconnect()
+    GIDSignIn.sharedInstance.signOut()
+  }
+
+  var authoriser: GTMFetcherAuthorizationProtocol?
+
+  override init() {
+    super.init()
+    GIDSignIn.sharedInstance.restorePreviousSignIn { [weak self] user, error in
+      self?.sign(GIDSignIn.sharedInstance, didSignInFor: user, withError: error)
+    }
+  }
+
+
+  /// Either add GoogleService-Info.plist to your project
+  /// or manually initialise Google Signin by calling
+  /// GIDSignIn.sharedInstance().clientID = "YOUR_CLIENT_ID" in your AppDelegate
+  var clientIDFromPlist:String {
+    let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
+    if let dict = NSDictionary(contentsOfFile: path ?? "") as? [String:Any]? {
+      let clientID = dict?["CLIENT_ID"] as? String
+      if let clientID = clientID  {
+        return clientID
+      }
+    }
+
+    fatalError("GoogleService-Info.plist hasn't been added to the project")
+  }
+
+
+
+  public func sign(_ signIn: GIDSignIn?, didSignInFor user: GIDGoogleUser?, withError error: Error?) {
+    if error == nil {
+      if user?.grantedScopes?.contains(kGTLRAuthScopeDrive) ?? false {
+        authoriser = user?.authentication.fetcherAuthorizer()
         NotificationCenter.default.post(name: HSGIDSignInHandler.hsGIDSignInChangedNotification, object: self)
+      } else {
+        guard let viewController = self.viewController else {
+          return
+        }
+        GIDSignIn.sharedInstance.addScopes(
+          [kGTLRAuthScopeDrive],
+          presenting: viewController) { [weak self] user, error in
+          self?.sign(GIDSignIn.sharedInstance, didSignInFor: user, withError: error)
+        }
+      }
+    } else {
+      authoriser = nil
+      NotificationCenter.default.post(name: HSGIDSignInHandler.hsGIDSignInFailedNotification, object: self)
+
+      //silent signin generates this error
+      if let code = (error as NSError?)?.code {
+        if code == GIDSignInError.hasNoAuthInKeychain.rawValue {
+          return
+        }
+      }
+
+      if let viewController = viewController {
+        let alert = UIAlertController.init(title: "Unable to sign in to Drive",
+                                           message: error?.localizedDescription,
+                                           preferredStyle: .alert)
+        alert.addAction(UIAlertAction.init(title: "OK", style: .default))
+        viewController.present(alert, animated: true)
+      }
     }
+  }
+
+  public func sign(_ signIn: GIDSignIn?, didDisconnectWith user: GIDGoogleUser?, withError error: Error?) {
+    print("User disconnected")
+    authoriser = nil
+    NotificationCenter.default.post(name: HSGIDSignInHandler.hsGIDSignInChangedNotification, object: self)
+  }
 }
